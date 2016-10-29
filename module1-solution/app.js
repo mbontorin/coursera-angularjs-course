1bc38a2db4209a772ba6d175429dec0bc971c546
mbontorin <mbontorin@gmail.com>
2016-10-29 21:19:35 -0200

    - adding missed case where nothing was provided as input - add different styling for nothing inserted and something inserted
    Signed-off-by: mbontorin <mbontorin@gmail.com>

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -9,9 +9,10 @@ function LunchCheckController($scope) {
   $scope.message = "";
   $scope.lunchListString = "";
 
-  //The getLunchList function uses a filter which will exclude from the
-  //lunch list any empty string. So, if user passes something like
-  //'a,,,b,,,c' in the text box, this will be only 3 lunches: a,b,c
+  //IMPORTANT: The getLunchList function uses a filter which will exclude from
+  //the lunch list any empty string. So, if user passes something like 'a,,b,,c'
+  //in the text box, this will be only 3 lunches: a,b,c. Also, a sequence of
+  //commas ',,,,,,' will be considered nothing, just an empty list.
   function getLunchList() {
     var tempList = $scope.lunchListString.split(",").filter(function (e) {
       if (e === "") {
@@ -26,13 +27,20 @@ function LunchCheckController($scope) {
 
   $scope.doCheckLunch = function () {
     var lunchList = getLunchList();
-    if (lunchList.length <= 3) {
+    if (lunchList.length == 0) {
+      $scope.message = "Please enter data first";
+      $scope.formStyle = {"border-color":"red"};
+      $scope.textStyle = {"color":"red"};
+    } else if (lunchList.length <= 3) {
       $scope.message = "Enjoy!"
+      $scope.formStyle = {"border-color":"green"};
+      $scope.textStyle = {"color":"green"};
     }
     else {
       $scope.message = "Too much!"
+      $scope.formStyle = {"border-color":"green"};
+      $scope.textStyle = {"color":"green"};
     }
   }
 }
-
 })();
