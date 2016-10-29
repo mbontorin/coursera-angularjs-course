608c9743eb555679dca2082621d57ee204da474c
mbontorin <mbontorin@gmail.com>
2016-10-29 20:32:01 -0200

    filter out empty strings from the lunch count
    Signed-off-by: mbontorin <mbontorin@gmail.com>

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,11 +7,26 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   $scope.message = "";
-  $scope.lunchList = "";
+  $scope.lunchListString = "";
+
+  //The getLunchList function uses a filter which will exclude from the
+  //lunch list any empty string. So, if user passes something like
+  //'a,,,b,,,c' in the text box, this will be only 3 lunches: a,b,c
+  function getLunchList() {
+    var tempList = $scope.lunchListString.split(",").filter(function (e) {
+      if (e === "") {
+        return false;
+      }
+      else {
+        return true;
+      }
+    });
+    return tempList;
+  }
 
   $scope.doCheckLunch = function () {
-    var tempList = $scope.lunchList.split(",");
-    if (tempList.length <= 3) {
+    var lunchList = getLunchList();
+    if (lunchList.length <= 3) {
       $scope.message = "Enjoy!"
     }
     else {
